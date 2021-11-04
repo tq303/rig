4a0c11dece4ca6352d40b286d1e133af3178a0f1
tq303 <an@ony.mous>
2021-11-04 11:50:40 +0000

    nvim & kitty & macos setup

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Indentation (global)
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 2 -- Indent width (used by >>, <<, etc.)
+vim.opt.tabstop = 2 -- How many spaces a <Tab> counts for
+vim.opt.softtabstop = 2 -- How many spaces <Tab>/<BS> use in Insert mode
+vim.opt.smartindent = true -- Auto-indent new lines intelligently
+vim.opt.autoindent = true -- Copy indent from current line when starting a new one
+vim.opt.number = true -- show absolute line numbers
+vim.opt.relativenumber = true -- show relative line numbers (optional, comment out if you don’t like it)
